54bca69bd0a70b068bb2ba1ce2f25e46fd0920be
TanninOne <seppleviathan@gmx.de>
2023-03-09 15:25:32 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
-let webpack = require('vortex-api/bin/webpack').default;
+const webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('gameversion-hash', __dirname, 4);
+module.exports = webpack('gameversion-hash', __dirname, 5);
